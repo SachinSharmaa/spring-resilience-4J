b0217a9384a9758610500c7c4f0943d1188b808a
Sachin Sharma <sachinsharmaa15@gmail.com>
2020-05-25 14:06:31 +0530

    Added config properties

--- src/main/java/com/example/resilience/poc/config/Resilience4JConfig.java
+++ src/main/java/com/example/resilience/poc/config/Resilience4JConfig.java
@@ -2,6 +2,7 @@ package com.example.resilience.poc.config;
 
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
@@ -13,20 +14,40 @@ import java.time.Duration;
 @Configuration
 public class Resilience4JConfig {
 
+    @Value("${resilience.failure.rate.threshold:50}")
+    private float failureRateThreshold;
+
+    @Value("${resilience.slow.call.duration.threshold:4000}")
+    private long slowCallDurationThreshold;
+
+    @Value("${resilience.wait.duration.in.open.state:20000}")
+    private long waitDurationInOpenState;
+
+    @Value("${resilience.minimum.number.of.calls:2}")
+    private int minimumNumberOfCalls;
+
+    @Value("${resilience.sliding.window.size:2}")
+    private int slidingWindowSize;
+
+    @Value("${resilience.permitted.number.of.calls.in.half.open.state:2}")
+    private int permittedNumberOfCallsInHalfOpenState;
+
+    @Value("${resilience.time.out.duration:10}")
+    private long timeoutDuration;
+
     @Bean
     public Customizer<Resilience4JCircuitBreakerFactory> globalCustomConfiguration() {
-
         CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.custom()
-                .failureRateThreshold(50)
-                .slowCallDurationThreshold(Duration.ofMillis(4000))
-                .waitDurationInOpenState(Duration.ofMillis(20000))
-                .minimumNumberOfCalls(2)
-                .slidingWindowSize(2)
-                .permittedNumberOfCallsInHalfOpenState(2)
+                .failureRateThreshold(failureRateThreshold)
+                .slowCallDurationThreshold(Duration.ofMillis(slowCallDurationThreshold))
+                .waitDurationInOpenState(Duration.ofMillis(waitDurationInOpenState))
+                .minimumNumberOfCalls(minimumNumberOfCalls)
+                .slidingWindowSize(slidingWindowSize)
+                .permittedNumberOfCallsInHalfOpenState(permittedNumberOfCallsInHalfOpenState)
                 .build();
 
         TimeLimiterConfig timeLimiterConfig = TimeLimiterConfig.custom()
-                .timeoutDuration(Duration.ofSeconds(10))
+                .timeoutDuration(Duration.ofSeconds(timeoutDuration))
                 .build();
 
         return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
